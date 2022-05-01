9a1b027bb59540eb8bf27c52ae15f3ea56c342e7
Ichika51 <yaroslaw.saly@yandex.ru>
2022-05-01 23:16:37 +0300

    Update Source.cpp
    Выполняет функции путем рекурсий
    1-Считает число Фибоначчи для числа
    2-Находит Пифагоровы тройки до нужного числа
    3-Рисует треугольник заданным размером
    4-Рисует ромб заданным размером
    5-Выход из программы

--- Source.cpp
+++ Source.cpp
@@ -6,6 +6,18 @@
 
 using namespace std;
 
+int check_switch() {//Проверка для ввода числа из 1 задания
+    int k;
+    cin >> k;
+    while (cin.fail() || k < 1 || k >= 6) {
+        cin.clear();
+        cin.ignore(32767, '\n');
+        cout << "\nНеккоректное число, повторите ввод(Ввести число нужно от 1 до 3): ";
+        cin >> k;
+    }
+    return k;
+}
+
 int check_figura() {//Проверка для ввода числа из 1 задания
     int k;
     cin >> k;
@@ -42,64 +54,26 @@ int check_pif() {//Проверка для ввода числа из 2 зада
     return k;
 }
 
-int fib(int n) {//1 задание
+int fib(int n) {
     if (n == 0) return 0;
     if (n == 1 || n == 2) return 1;
     return fib(n - 1) + fib(n - 2);
 }
 
-int pifagor(int pif){
-
-    int a2 = pif;
-        
-    for (int x = 1; x < a2; x++){
-        for (int y = 1; y <a2; y++){
-            for (int z = 1; z < a2; z++){
-                if (x * x + y * y == z * z) {
-                    return pifagor(pif - 1);
-                    cout << "{ " << x << ";" << y << ";" << z << "}" << std::endl;
-                }
-            }
+int pifagor(int pif,int m, int n) {
+    clock_t t0 = clock();
+    if (m * m < pif) {
+        if (m * m + n * n < pif) {
+            cout << m * m - n * n << " " << 2 * n * m << " " << m * m + n * n << endl;
         }
     }
-}
-
-int pifagor_test(int pif) {
-
-    int a2 = pif;
-
-
-
-    for (int x = 1; x < a2; x++) {
-        for (int y = 1; y < a2; y++) {
-            for (int z = 1; z < a2; z++) {
-                if (x * x + y * y == z * z) {
-                    return pifagor(pif - 1);
-                    cout << "{ " << x << ";" << y << ";" << z << "}" << std::endl;
-                }
-            }
-        }
+    else {
+        clock_t t1 = clock();
+        printf("%ims\n", (int)(t1 - t0) * 1000 / CLOCKS_PER_SEC);
+        return 0;
     }
+    return pifagor(pif, m+1, n+1);
 }
-
-//int pifagor_recursiya(int pif) {
-//    clock_t t0 = clock();
-//    int i = 0;
-//    for (int z = 1;z<pif ; ++z)
-//        for (int x = 1; x <= z; ++x)
-//            for (int y = x; y <= z; ++y)
-//                if (x * x + y * y == z * z) {
-//                    printf("(%i,%i,%i)\n", x, y, z);
-//                    if (++i == 100)
-//                        break;
-//                        goto done;
-//                }
-//done:
-//
-//    clock_t t1 = clock();
-//    printf("%ims\n", (int)(t1 - t0) * 1000 / CLOCKS_PER_SEC);
-//    return 0;
-//}
 
 int piramidka(int tr) {
     for (int i = 0; i < tr;i++) {
@@ -127,32 +101,52 @@ void out_stars(unsigned int count, unsigned int max_count){
         }
 }
 
-void out_stars(unsigned int max_stars){
+void out_stars_s(unsigned int max_stars){
     if (max_stars != 0)
         out_stars(0, max_stars);
 }
 
 int main() {
     setlocale(LC_ALL, "RUS");
-    
-    /*2 ЗАДАНИЕ*/
-    cout << "Введите максимальное число Пифагоровых троек: " << endl;
-    int pif = check_pif();
-    //pifagor_recursiya(pif);
-    pifagor(pif);
-
-    /*1 ЗАДАНИЕ*/
-    //cout << "Введите число для задания 1: " << endl;
-    //int n=check_fib();
-    //int f = fib(n);
-    //cout << "Число Фибаначи для задания 1: " << f << endl;
-
-    /*3 ЗАДАНИЕ*/
-    //cout << "Введите размер треугольника: " << endl;
-    //int tr = check_figura();
-    //piramidka(tr);
-    //cout << "Введите размер ромба: " << endl;
-    //int romb = check_figura();
-    //out_stars(romb);
-    return 0;
+    while (1) {
+        cout << "Введите:"<<endl<<"1-Число Фибоначчи;"<<endl<<"2-Пифигоровы тройки;"<<endl<<"3-Нарисовать треугольник;"<<endl<<"4-Нарисовать ромб;"<<endl<<"5-Выход;" << endl;
+        int d = check_switch();
+        switch (d) {
+        case(1):
+        {
+            cout << "Введите число, чтобы найти значение Фибоначчи: " << endl;
+            int num = check_fib();
+            int f = fib(num);
+            cout << "Число Фибаначи: " << f << endl;
+            break;
+        }
+        case(2):
+        {
+            int m = 2;
+            int n = 1;
+            cout << "Введите максимальное число Пифагоровых троек: " << endl;
+            int pif = check_pif();
+            pifagor(pif, m, n);
+            break;
+        }
+        case(3):
+        {
+            cout << "Введите размер треугольника: " << endl;
+            int tr = check_figura();
+            piramidka(tr);
+            break;
+        }
+        case(4):
+        {
+            cout << "Введите размер ромба: " << endl;
+            int romb = check_figura();
+            out_stars_s(romb);
+            break;
+        }
+        case(5):
+        {
+            return 0;
+        }
+        } 
+    } 
 }
